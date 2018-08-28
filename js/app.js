ae9a0b5bcb36d15123abb449cac9b023051e76b0
GatoVagabundo <contactogatovagabundo@gmail.com>
2018-08-28 12:48:37 -0500

    Subiendo cambios en js

--- js/app.js
+++ js/app.js
@@ -1,37 +1,33 @@
-function showModal(evento) {
-    $('#cancelSkills').click('shown.bs.modal', function() {
-        var eventClick = evento.target.id;
-        console.log(eventClick);
-    });
-}
+ $('#cancelSkills').click('shown.bs.modal', function(evento) {
+     var id = evento.target.id;
+     console.log(id);
+     document.getElementById("#remove").setAtrribute("skill", id);
 
-var buttons = document.querySelectorAll('.tetxBtn')
-var remove = document.querySelectorAll('#remove')
+     $('#remove').click("removeBtn", function(e) {
+         var botonRemove = e.target.dataset.skills
+         console.log(botonRemove);
+     });
 
-
-for (var i = 0; i < buttons.length; i++) {
-
-    buttons[i].addEventListener('click', showModal);
-}
-
-
-function removed(evento) {
-
-}
+ });
 
 
 
 
 
 
+ //  var buttons = document.querySelectorAll('.tetxBtn')
+ //  var remove = document.querySelectorAll('#remove')
 
-// $('.btn').click('shown.bs.modal', function(e) {
-//             var elemento = e.target
-//             console.log(elemento)
-//             if (elemento === $('#remove').click(function()) {
-//                     e.target.style.visibility = 'hidden';
-//                 }
-//             })
+
+ //  for (var i = 0; i < buttons.length; i++) {
+
+ //      buttons[i].addEventListener('click', showModal);
+ //  }
+
+
+ //  function removed(evento) {
+
+ //  }
 
 
 
@@ -39,72 +35,86 @@ function removed(evento) {
 
 
 
-
-// function general() {
-//     $('.btn').click('shown.bs.modal', function(e){
-//         var elemento = e.target
-//        elemento = document.createElementByClassName("remove");
-//        var padre = document.getElementById("cancelSkills");
-//        elemento.appendChild(padre);
-
-//         var classElement = e.target.className;
-//         console.log(classElement)
-
-//     return elemento;
-// })
-// }
-
-// $('#remove').click(function(e){
-//     var removeElement = document.getElementsByClassName("remove");
-//     removeElement.remove();
-//      console.log(removeElement);
-
-// })
-// $('.btn').click('shown.bs.modal', function(e){
-//     var elemento = e.target
-
-//     var classElement = e.target.className;
-//     console.log(classElement);
-//     if($('#remove').click(function(e){
-//         return true
-//     }){
-
-//     }
-
-// })
-
-// $('#remove').click(function(e, classElement){
-//     classElement.remove();
-//      console.log(classElement);
-
-// })
+ // $('.btn').click('shown.bs.modal', function(e) {
+ //             var elemento = e.target
+ //             console.log(elemento)
+ //             if (elemento === $('#remove').click(function()) {
+ //                     e.target.style.visibility = 'hidden';
+ //                 }
+ //             })
 
 
 
-// //  })
-
-// $('#cancelSkills').click('shown.bs.modal', function() {
-//     console.log("prueba");
-// })
-
-// $('#cancelSkills').click(function remove(e) {
 
 
-//     var elemento = e.target
-//  console.log(elemento);
-
-//  })
-
-//       if (e.srcElement)
-//       tag = e.srcElement.tagName;
-//   else if (e.target)
-//         tag = e.target.tagName;
-
-//   alert("El elemento selecionado ha sido " + tag);
 
 
-// $('#remove').click(function remove(e) {
-//     var id = e.target.id;
-// })
 
-//recuperar elemento cliqueado y quitarlo de html
+ // function general() {
+ //     $('.btn').click('shown.bs.modal', function(e){
+ //         var elemento = e.target
+ //        elemento = document.createElementByClassName("remove");
+ //        var padre = document.getElementById("cancelSkills");
+ //        elemento.appendChild(padre);
+
+ //         var classElement = e.target.className;
+ //         console.log(classElement)
+
+ //     return elemento;
+ // })
+ // }
+
+ // $('#remove').click(function(e){
+ //     var removeElement = document.getElementsByClassName("remove");
+ //     removeElement.remove();
+ //      console.log(removeElement);
+
+ // })
+ // $('.btn').click('shown.bs.modal', function(e){
+ //     var elemento = e.target
+
+ //     var classElement = e.target.className;
+ //     console.log(classElement);
+ //     if($('#remove').click(function(e){
+ //         return true
+ //     }){
+
+ //     }
+
+ // })
+
+ // $('#remove').click(function(e, classElement){
+ //     classElement.remove();
+ //      console.log(classElement);
+
+ // })
+
+
+
+ // //  })
+
+ // $('#cancelSkills').click('shown.bs.modal', function() {
+ //     console.log("prueba");
+ // })
+
+ // $('#cancelSkills').click(function remove(e) {
+
+
+ //     var elemento = e.target
+ //  console.log(elemento);
+
+ //  })
+
+ //       if (e.srcElement)
+ //       tag = e.srcElement.tagName;
+ //   else if (e.target)
+ //         tag = e.target.tagName;
+
+ //   alert("El elemento selecionado ha sido " + tag);
+
+
+ // $('#remove').click(function remove(e) {
+ //     var id = e.target.id;
+ // })
+
+ //recuperar elemento cliqueado y quitarlo de html
